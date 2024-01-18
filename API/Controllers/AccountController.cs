425d93499ee7a6f0cf41f3ffe3a30951da6917b0
mohammedterfa <mohammedterfa@gmail.com>
2024-01-18 15:04:34 +0300

    code

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -9,7 +9,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-public class AccountController: BaseApiController
+public class AccountController : BaseApiController
 {
     private readonly DataContext _context;
     private readonly ITokenService _tokenService;
@@ -20,12 +20,11 @@ public class AccountController: BaseApiController
         _tokenService = tokenService;
     }
 
-    [HttpPost("register")] //POST:api/account/register
+    [HttpPost("register")]
     public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
     {
+        if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
 
-        if(await UserExists(registerDto.Username)) return BadRequest("Username is taken");
-      
         using var hmac = new HMACSHA512();
 
         var user = new AppUser
@@ -36,7 +35,6 @@ public class AccountController: BaseApiController
         };
 
         _context.Users.Add(user);
-
         await _context.SaveChangesAsync();
 
         return new UserDto
@@ -49,17 +47,18 @@ public class AccountController: BaseApiController
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-        var user = await _context.Users.SingleOrDefaultAsync(x =>
-            x.UserName == loginDto.Username);
+        var user = await _context.Users
+            .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
-        if(user == null) return Unauthorized("invalid username");
+        if (user == null) return Unauthorized("Invalid username");
 
         using var hmac = new HMACSHA512(user.PasswordSalt);
 
         var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
 
-        for (int i = 0; i < computedHash.Length; i++){
-            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("invalid password");
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
         }
 
         return new UserDto
@@ -67,13 +66,10 @@ public class AccountController: BaseApiController
             Username = user.UserName,
             Token = _tokenService.CreateToken(user)
         };
-
     }
 
     private async Task<bool> UserExists(string username)
     {
         return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
     }
-
-
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,10 @@
+using System.Text;
 using API.Data;
 using API.Interfaces;
 using API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +20,17 @@ builder.Services.AddCors();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+  .AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+      ValidateIssuerSigningKey = true,
+      IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["TokenKey"])),
+      ValidateIssuer = false,
+      ValidateAudience = false
+    };
+  });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,7 +44,11 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+  .WithOrigins("https://localhost:4200"));
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
