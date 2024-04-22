54f5f74580a073536203c9f7793509bf96538bbf
mohammedterfa <mohammedterfa@gmail.com>
2024-04-22 18:00:52 +0300

    adding pagination for the likes

--- API/Data/LikesRepository.cs
+++ API/Data/LikesRepository.cs
@@ -1,6 +1,7 @@
 ﻿using API.DTOs;
 using API.Entities;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.EntityFrameworkCore;
 
@@ -18,28 +19,30 @@ public class LikesRepository : ILikesRepository
         return await _context.Likes.FindAsync(sourceUserId, targetUserId);
     }
 
-    public async Task<IEnumerable<LikeDto>> GetUserLikes(string predicate, int userId)
+    public async Task<PagedList<LikeDto>> GetUserLikes(LikesParams likesParams)
     {
         var users = _context.Users.OrderBy(u => u.UserName).AsQueryable();
         var likes = _context.Likes.AsQueryable();
-        if(predicate == "liked")
+        if(likesParams.Predicate == "liked")
         {
-            likes = likes.Where(like => like.SourceUserId == userId);
+            likes = likes.Where(like => like.SourceUserId == likesParams.UserId);
             users = likes.Select(like => like.TargetUser);
         }
-        if(predicate == "likedBy")
+        if(likesParams.Predicate == "likedBy")
         {
-            likes = likes.Where(like => like.TargetUserId == userId);
+            likes = likes.Where(like => like.TargetUserId == likesParams.UserId);
             users = likes.Select(like => like.SourceUser);
         }
-        return await users.Select(user => new LikeDto{
+        var LikedUsers =  users.Select(user => new LikeDto{
             UserName = user.UserName,
             KnownAs = user.KnownAs,
             Age = user.DateOfBirth.CalcuateAge(),
             PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain).Url,
             City = user.City,
             Id = user.Id,
-        }).ToListAsync();
+        });
+
+        return await PagedList<LikeDto>.CreateAsync(LikedUsers, likesParams.PageNumber, likesParams.PageSize);
     }
 
     public async Task<AppUser> GetUserWithLikes(int userId)
--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76ae1ea4c6a5961705b2cc757d484513fe8d2623")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92e2a1185dd541fea83952ee839bd69dbbd64a85")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
