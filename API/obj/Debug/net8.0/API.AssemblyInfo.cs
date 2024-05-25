51452378d6c3d76243562721a8639b7631bd59c8
mohammedterfa <mohammedterfa@gmail.com>
2024-05-25 20:00:41 +0300

    setting the enitities for messging

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc753bec5fc9a039aed22e3bb4ca8bb57262e542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40320629704b8bd9ebe913385414e12ca8f8e410")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
