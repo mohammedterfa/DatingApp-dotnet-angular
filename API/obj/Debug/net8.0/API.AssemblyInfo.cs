9c0ba4d4e9e95401094b65f5751b361f89525fba
mohammedterfa <mohammedterfa@gmail.com>
2024-08-07 11:25:32 +0300

    deleting messages in the API

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85aadd638145319e9e9f0e660e3193b5c232eed2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9691e5147c528548b908d3c45371d13ec3425ebf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -15,6 +15,7 @@ export class MessagesComponent implements OnInit  {
   container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private  messageSevice: MessageService) { }
   ngOnInit(): void {
@@ -24,10 +25,12 @@ export class MessagesComponent implements OnInit  {
 
   loadMessages()
   {
+    this.loading = true;
     this.messageSevice.getMessages(this.pageNumber, this.pageSize, this.container).subscribe({
       next: response => {
         this.messages = response.result;
         this.pagination = response.pagination;
+        this.loading = false;
       }
     })
   }
