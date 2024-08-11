6d9c2ee26c34aac5c1b3c1711963876eaaa4c6af
mohammedterfa <mohammedterfa@gmail.com>
2024-08-11 20:33:02 +0300

    adding new project

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c0ba4d4e9e95401094b65f5751b361f89525fba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e061027e2ce169ae97298dfb4c7ec2a24cb4132c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
