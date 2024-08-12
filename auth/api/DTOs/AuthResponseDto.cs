89c1f7d080d8eb0528dc2e53e31ebd6e2a70523b
mohammedterfa <mohammedterfa@gmail.com>
2024-08-12 11:44:42 +0300

    adding register api

---
+++ auth/api/DTOs/AuthResponseDto.cs
@@ -0,0 +1,8 @@
+namespace api.DTOs;
+
+public class AuthResponseDto
+{
+    public string? Token { get; set; } =string.Empty;
+    public bool IsSuccess { get; set; }
+    public string? Message { get; set; }
+}
