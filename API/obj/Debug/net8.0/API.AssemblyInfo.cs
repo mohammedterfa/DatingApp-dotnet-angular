9c8825e0f714510c68de54b12d2e21832d50f77a
mohammedterfa <mohammedterfa@gmail.com>
2024-08-25 12:30:52 +0300

    config DBContext to user IdentityFrameWork

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ec7805fabc0af56fea0ac0d6fd2ba7d07a04fe0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0ca5681b74a100a10dc6299fd0cec4254f25583")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
