c0ca5681b74a100a10dc6299fd0cec4254f25583
mohammedterfa <mohammedterfa@gmail.com>
2024-08-25 12:16:53 +0300

    setting up entities to use IdentityFramework

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -31,12 +31,7 @@ public class AccountController : BaseApiController
 
         var user = _mapper.Map<AppUser>(registerDto);
 
-        using var hmac = new HMACSHA512();
-
         user.UserName = registerDto.Username.ToLower();
-        user.PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password));
-        user.PasswordSalt = hmac.Key;
- 
 
         _context.Users.Add(user);
         await _context.SaveChangesAsync();
@@ -58,15 +53,6 @@ public class AccountController : BaseApiController
             .SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
 
         if (user == null) return Unauthorized("Invalid username");
-
-        using var hmac = new HMACSHA512(user.PasswordSalt);
-
-        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
-
-        for (int i = 0; i < computedHash.Length; i++)
-        {
-            if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
-        }
 
         return new UserDto
         {
