8986118dd19bfe6cfb7c622a386c69873d50fda0
mohammedterfa <mohammedterfa@gmail.com>
2024-09-03 12:07:03 +0300

    refactroing and adding a new migrations

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b2bb7942991ab8859bbe2d1fc484dd283a800f0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8549d6ba8903f5940812f4c05663877124a6de11")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
