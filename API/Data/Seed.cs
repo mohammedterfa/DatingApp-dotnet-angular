0737ab8c98582f3578aaad3687edbe004a00f87a
mohammedterfa <mohammedterfa@gmail.com>
2024-09-22 20:33:50 +0300

    Adding roles to the app

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -9,7 +9,8 @@ namespace API.Data;
 
 public class Seed
 {
-    public static async Task SeedUsers(UserManager<AppUser> userManager)
+    public static async Task SeedUsers(UserManager<AppUser> userManager, 
+        RoleManager<AppRole> roleManager)
     {
         if (await userManager.Users.AnyAsync()) return;
 
@@ -17,7 +18,19 @@ public class Seed
 
         var options = new JsonSerializerOptions{PropertyNameCaseInsensitive = true};
 
-        var users = JsonSerializer.Deserialize<List<AppUser>>(userData, options);
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+
+        var roles = new List<AppRole>
+        {
+            new AppRole{Name = "Member"},
+            new AppRole{Name = "Admin"},
+            new AppRole{Name = "Moderator"},
+        };
+
+        foreach (var role in roles)
+        {
+            await roleManager.CreateAsync(role);
+        }
 
         foreach (var user in users)
         {
@@ -25,7 +38,17 @@ public class Seed
             user.UserName = user.UserName.ToLower();
 
             await userManager.CreateAsync(user, "P@ssw0rd");
+
+            await userManager.AddToRoleAsync(user, "Member");
         }
+
+        var admin  = new AppUser{
+            UserName = "admin"
+        };
+
+        await userManager.CreateAsync(admin, "P@ssw0rd");
+
+        await userManager.AddToRolesAsync(admin, new[]{"Admin", "Moderator"});
 
         
     }
