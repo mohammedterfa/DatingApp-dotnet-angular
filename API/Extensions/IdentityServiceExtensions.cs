77254f7c9700ff9ebc27fb73b013488582ed512a
mohammedterfa <mohammedterfa@gmail.com>
2024-10-06 11:46:42 +0300

    Adding policy based authorisation

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -32,6 +32,11 @@ public static class IdentityServiceExtensions
                     ValidateAudience = false
                     };
                 });
+        
+        services.AddAuthorization(opt => {
+            opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+            opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
+        });
 
         return services;
     }
