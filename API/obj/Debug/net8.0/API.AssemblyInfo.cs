da709090ea1c59b74b75fb0424d81cb4d5b5343d
mohammedterfa <mohammedterfa@gmail.com>
2024-10-13 19:41:25 +0300

    adding an admin component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77254f7c9700ff9ebc27fb73b013488582ed512a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38188a4fb4518c648543e354934844657e7ada0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
