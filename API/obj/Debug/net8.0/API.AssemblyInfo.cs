1ddb3676aae6b0884401ceeb2dc35da1dad90691
mohammedterfa <mohammedterfa@gmail.com>
2024-10-16 11:51:29 +0300

    adding custom dirctive

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da709090ea1c59b74b75fb0424d81cb4d5b5343d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23cc9b531ee3660ed26c3e34ae67470cb339efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
