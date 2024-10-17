e3b1d387a52e6c20a02d1a0b415ad289e25c957e
mohammedterfa <mohammedterfa@gmail.com>
2024-10-17 12:37:05 +0300

    adding the edit role component

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b23cc9b531ee3660ed26c3e34ae67470cb339efa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ddb3676aae6b0884401ceeb2dc35da1dad90691")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
