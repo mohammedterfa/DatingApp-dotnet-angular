3bb6502fa93094c9d1ae0fbb123b444a2cba439a
mohammedterfa <mohammedterfa@gmail.com>
2024-10-20 12:20:00 +0300

    client side singalR

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed6f2e239b207aa7c4f221f86dc36ce8ac955f7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1786f44bb3cb5e96fc318198220bdf0a8564ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
